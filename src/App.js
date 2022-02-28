bf1aedd500cbb927908499643553dfb5a5dbb652
Crystal <soo9028@gmail.com>
2022-02-28 14:55:47 +0900

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import React, { Component } from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      Hello World
+    </div>
+  );
+}
+
+export default App;
