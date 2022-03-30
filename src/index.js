50ba3e96b10be54a504f6f121747954b2f63ba96
Crystal <soo9028@gmail.com>
2022-03-30 11:52:22 +0900

    Feat:calculation functions and minor css edits

--- src/index.js
+++ src/index.js
@@ -3,7 +3,9 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import 'bootstrap/dist/css/bootstrap.min.css';
+// import 'bootstrap/dist/css/bootstrap.min.css';
+import 'mdb-ui-kit/css/mdb.min.css';
+
 
 
 ReactDOM.render(
